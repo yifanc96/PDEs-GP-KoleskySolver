55ca51883a0780af5129c790e3572b1142acd637
Yifan Chen <yifanc96@gmail.com>
2022-01-19 17:49:24 -0800

    pre-allocation of U transpose - much faster

--- FastCholSolver_NonLinElliptic2d.jl
+++ FastCholSolver_NonLinElliptic2d.jl
@@ -1,7 +1,7 @@
 # For linear algebra
 using StaticArrays: SVector 
 using LinearAlgebra
-using SparseArrays: SparseMatrixCSC 
+using SparseArrays: SparseMatrixCSC, sparse
 
 # Fast Cholesky
 using KoLesky 
@@ -19,11 +19,11 @@ using Profile
 
 ## PDEs type
 abstract type AbstractPDEs end
-struct NonlinElliptic2d <: AbstractPDEs
+struct NonlinElliptic2d{Tα,Tm,TΩ} <: AbstractPDEs
     # eqn: -Δu + α*u^m = f in [Ω[1,1],Ω[2,1]]*[Ω[1,2],Ω[2,2]]
-    α::Float64
-    m::Int64
-    Ω::Matrix{Float64}
+    α::Tα
+    m::Tm
+    Ω::TΩ
     bdy::Function
     rhs::Function
 end
@@ -128,6 +128,7 @@ abstract type implicit_mtx end
 struct approx_Theta_train{Tv,Ti,Tmtx<:SparseMatrixCSC{Tv,Ti}} <: implicit_mtx
     P::Vector{Ti}
     U::Tmtx
+    L::Tmtx
     δ_coefs::Vector{Tv}
     N_boundary::Ti
     N_domain::Ti
@@ -140,6 +141,7 @@ end
 struct precond_Theta_train{Tv,Ti,Tmtx<:SparseMatrixCSC{Tv,Ti}} <: implicit_mtx
     P::Vector{Ti}
     U::Tmtx
+    L::Tmtx
 end
 
 function size(A::approx_Theta_train, num)
@@ -148,7 +150,7 @@ end
 
 function mul!(x, Θtrain::approx_Theta_train, b)
     temp = vcat(b[1:Θtrain.N_boundary],Θtrain.δ_coefs.*b[Θtrain.N_boundary+1:end],b[Θtrain.N_boundary+1:end])
-    temp = Θtrain.U'\(Θtrain.U\temp[Θtrain.P])
+    temp = Θtrain.L\(Θtrain.U\temp[Θtrain.P])
     temp[Θtrain.P] = temp
 
     @views x[1:Θtrain.N_boundary] = temp[1:Θtrain.N_boundary]
@@ -156,7 +158,7 @@ function mul!(x, Θtrain::approx_Theta_train, b)
 end
 
 function ldiv!(x, precond::precond_Theta_train, b)
-    x[precond.P] = precond.U*(precond.U'*b[precond.P])
+    x[precond.P] = precond.U*(precond.L*b[precond.P])
 end
 
 function iterGPR_fast_pcg(eqn, cov, X_domain, X_boundary, sol_init, nugget, GNsteps; ρ_big =4.0, ρ_small=6.0, k_neighbors = 4, lambda = 1.5, alpha = 1.0)
@@ -185,17 +187,18 @@ function iterGPR_fast_pcg(eqn, cov, X_domain, X_boundary, sol_init, nugget, GNst
     @printf("[Big Theta: implicit factorization] time")
     @time implicit_bigΘ = KoLesky.ImplicitKLFactorization_FollowDiracs(cov, measurements, ρ_big, k_neighbors; lambda = lambda, alpha = alpha)
 
-    @printf("[Big Theta: implicit factorization] length of row indices")
-    @show length.(KoLesky.row_indices.(implicit_bigΘ.supernodes.supernodes))
-    @printf("[Big Theta: implicit factorization] length of column indices")
-    @show length.(KoLesky.column_indices.(implicit_bigΘ.supernodes.supernodes))
+    # @printf("[Big Theta: implicit factorization] length of row indices")
+    # @show length.(KoLesky.row_indices.(implicit_bigΘ.supernodes.supernodes))
+    # @printf("[Big Theta: implicit factorization] length of column indices")
+    # @show length.(KoLesky.column_indices.(implicit_bigΘ.supernodes.supernodes))
 
     @printf("[Big Theta: explicit factorization] time")
     @time explicit_bigΘ = KoLesky.ExplicitKLFactorization(implicit_bigΘ; nugget = nugget)
     U_bigΘ = explicit_bigΘ.U
+    L_bigΘ = sparse(U_bigΘ')
     P_bigΘ = explicit_bigΘ.P
 
-    Θtrain = approx_Theta_train(P_bigΘ, U_bigΘ, zeros(N_domain),N_boundary,N_domain)
+    Θtrain = approx_Theta_train(P_bigΘ, U_bigΘ, L_bigΘ,zeros(N_domain),N_boundary,N_domain)
 
     for step in 1:GNsteps
         
@@ -211,34 +214,38 @@ function iterGPR_fast_pcg(eqn, cov, X_domain, X_boundary, sol_init, nugget, GNst
         @printf("[Theta Train: implicit factorization] time")
         @time implicit_factor = KoLesky.ImplicitKLFactorization(cov, measurements, ρ_small, k_neighbors; lambda = lambda, alpha = alpha)
 
-        @printf("[Theta Train:implicit factorization] length of row indices")
-        @show length.(KoLesky.row_indices.(implicit_factor.supernodes.supernodes))
-        @printf("[Theta Train:implicit factorization] length of column indices")
-        @show length.(KoLesky.column_indices.(implicit_factor.supernodes.supernodes))
+        # @printf("[Theta Train:implicit factorization] length of row indices")
+        # @show length.(KoLesky.row_indices.(implicit_factor.supernodes.supernodes))
+        # @printf("[Theta Train:implicit factorization] length of column indices")
+        # @show length.(KoLesky.column_indices.(implicit_factor.supernodes.supernodes))
 
         @printf("[Theta Train: explicit factorization] time")
         @time explicit_factor = KoLesky.ExplicitKLFactorization(implicit_factor; nugget = nugget)
 
         U = explicit_factor.U
+        L = sparse(U')
         P = explicit_factor.P
         rhs_now = vcat(bdy, rhs.+eqn.α*(eqn.m-1)*sol_now.^eqn.m)
 
         # use the approximate solution as the initial point for the pCG iteration
-        sol = U*(U'*rhs_now[P])
+        sol = U*(L*rhs_now[P])
         sol[P] = sol
         # pcg step for Theta_train\rhs
 
-        @time Θtrain.δ_coefs .= δ_coefs_int
+        Θtrain.δ_coefs .= δ_coefs_int
         # Θtrain = approx_Theta_train(P_bigΘ, U_bigΘ, δ_coefs_int,N_boundary,N_domain)
+        # x = similar(rhs_now)
+        # @time mul!(x, Θtrain::approx_Theta_train, rhs_now)
 
-        precond = precond_Theta_train(P,U)
+        precond = precond_Theta_train(P,U,L)
         @printf "[pcg started]\n"
-        @time sol, ch = cg!(sol, Θtrain, rhs_now; Pl = precond, log=true, reltol=1e-5)
+        @time sol, ch = cg!(sol, Θtrain, rhs_now; Pl = precond, log=true)
+
         println("[pcg finished]",ch)
         
         # get approximated sol_now = Theta_test * sol
         temp = vcat(sol[1:N_boundary], δ_coefs_int .* sol[N_boundary+1:end], sol[N_boundary+1:end])
-        temp = U_bigΘ'\(U_bigΘ\temp[P_bigΘ]) 
+        temp = L_bigΘ\(U_bigΘ\temp[P_bigΘ]) 
         temp[P_bigΘ] = temp
         @views sol_now = temp[N_boundary+1:N_domain+N_boundary] 
     end
@@ -279,7 +286,7 @@ end
 @printf "[solver started] NonlinElliptic2d\n"
 eqn = NonlinElliptic2d(α,m,Ω,fun_bdy,fun_rhs)
 
-h_in = 0.02; h_bd = h_in
+h_in = 0.01; h_bd = h_in
 X_domain, X_boundary = sample_points_grid(eqn, h_in, h_bd)
 # X_domain, X_boundary = sample_points_rdm(eqn, 900, 124)
 N_domain = size(X_domain,2)
@@ -288,33 +295,42 @@ N_boundary = size(X_boundary,2)
 
 lengthscale = 0.2
 cov = KoLesky.MaternCovariance5_2(lengthscale)
+# cov = KoLesky.GaussianCovariance(lengthscale)
 @printf "[kernel] kernel is MaternCovariance5_2, lengthscale %.2f\n" lengthscale
 
 nugget = 1e-14
 @printf "[nugget] nugget term %e\n" nugget
 
-GNsteps = 2
+GNsteps_approximate = 3
 @printf "[total GN steps] %d\n" GNsteps
 
 ρ_big = 3.0
-ρ_small = 6.0
-k_neighbors = 4
+ρ_small = 2.0
+
+# ρ_big = 5.0
+# ρ_small = 5.0
+k_neighbors = 2
 @printf "[Fast Cholesky] ρ_big = %.2f, ρ_small = %.2f, k_neighbors = %d\n" ρ_big ρ_small k_neighbors
 
 sol_init = randn(N_domain) # initial solution
 truth = [fun_u(X_domain[:,i]) for i in 1:N_domain]
 
-@time sol = iterGPR_fast_pcg(eqn, cov, X_domain, X_boundary, sol_init, nugget, GNsteps; ρ_big = ρ_big, ρ_small = ρ_small, k_neighbors=k_neighbors);
+# @profview 
+@time sol = iterGPR_fast_pcg(eqn, cov, X_domain, X_boundary, sol_init, nugget, GNsteps_approximate; ρ_big = ρ_big, ρ_small = ρ_small, k_neighbors=k_neighbors);
 pts_accuracy = sqrt(sum((truth-sol).^2)/N_domain)
 println("[L2 accuracy: pCG method]",pts_accuracy)
 pts_max_accuracy = maximum(abs.(truth-sol))
 println("[Linf accuracy: pCG method]",pts_max_accuracy)
 
-@time sol_exact = iterGPR_exact(eqn, cov, X_domain, X_boundary, sol_init, nugget, GNsteps)
-pts_accuracy_exact = sqrt(sum((truth-sol_exact).^2)/N_domain)
-println("[L2 accuracy: exact method]", pts_accuracy_exact)
-pts_max_accuracy_exact = maximum(abs.(truth-sol_exact))
-println("[Linf accuracy: exact method]", pts_max_accuracy_exact)
 
+# GNsteps_exact = 3
+# @time sol_exact = iterGPR_exact(eqn, cov, X_domain, X_boundary, sol_init, nugget, GNsteps_exact)
+# pts_accuracy_exact = sqrt(sum((truth-sol_exact).^2)/N_domain)
+# println("[L2 accuracy: exact method]", pts_accuracy_exact)
+# pts_max_accuracy_exact = maximum(abs.(truth-sol_exact))
+# println("[Linf accuracy: exact method]", pts_max_accuracy_exact)
+
+# using PProf
+# pprof(;webport=58699)
 
 
